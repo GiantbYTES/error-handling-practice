ab0c88f83d5607ad8282d13e30ff2c21058d4488
Assaf Mor <asafmor14@gmail.com>
2025-07-16 12:08:03 +0300

    Modified isValidCommand in validation.js and fixed bugs

--- contact-manager/contacts.js
+++ contact-manager/contacts.js
@@ -23,49 +23,76 @@ function handleChoice(choise) {
         ui.handleError(err);
       }
       break;
+
     case "delete":
       console.log("delete");
-      validation.validEmail(process.argv[3]);
-      const result = contactsDB.deleteContact(process.argv[3]);
-      fileUtils.writeToFile("../contacts.json", contactsDB);
-      ui.handleDelete(process.argv[3], result, contactsDB.contactsList.length);
+      try {
+        validation.validEmail(process.argv[3]);
+        const result = contactsDB.deleteContact(process.argv[3]);
+        fileUtils.writeToFile("../contacts.json", contactsDB);
+        ui.handleDelete(
+          process.argv[3],
+          result,
+          contactsDB.contactsList.length
+        );
+      } catch (err) {
+        ui.handleError(err);
+      }
       break;
+
     case "list":
       console.log("list");
-      ui.handleDelete(contactsDB.getContactList());
+      try {
+        ui.handleDelete(contactsDB.getContactList());
+      } catch (err) {
+        ui.handleError(err);
+      }
       break;
+
     case "search":
       console.log("search");
-      let toReturn;
-      let type;
-      if (validation.isEmail(process.argv[3])) {
-        validation.validEmail(process.argv[3]);
-        type = "Email";
-        toReturn = contactsDB.getContactByEmail;
-      } else if (validation.isName(process.argv[3])) {
-        validation.validName(process.argv[3]);
-        type = "Name";
-        toReturn = contactsDB.getContactByName;
+      try {
+        let toReturn;
+        let type;
+        if (validation.isEmail(process.argv[3])) {
+          validation.validEmail(process.argv[3]);
+          type = "Email";
+          toReturn = contactsDB.getContactByEmail;
+        } else if (validation.isName(process.argv[3])) {
+          validation.validName(process.argv[3]);
+          type = "Name";
+          toReturn = contactsDB.getContactByName;
+        }
+        ui.handleSearch(type, toReturn, contactsDB.contactsList.length);
+      } catch (err) {
+        ui.handleError(err);
       }
-      ui.handleSearch(type, toReturn, contactsDB.contactsList.length);
       break;
+
     case "help":
       console.log("help");
       ui.handleHelp();
       break;
+
     default:
-      throw new Error(
-        "✗ Error: Unknown command 'invalidcommand'\n Usage: node contacts.js [add|list|search|delete|help] [arguments]"
-      );
+      console.log("ERROR!");
+      break;
   }
 }
 
 function run() {
-  // validation.isValidCommand(process.argv);
+  try {
+    validation.isValidCommand(process.argv);
+  } catch (err) {
+    ui.handleError(err);
+    return;
+  }
+
   // still doesn't work when file doesn't exist
   contactsDB.contactsList =
-    fileUtils.readFromFile("../contacts.json").contactsList;
+  fileUtils.readFromFile("../contacts.json").contactsList;
   let choice = process.argv[2];
   handleChoice(choice);
 }
+
 run();
